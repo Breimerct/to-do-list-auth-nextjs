140a9cd703eb76a011c36c89ffcfd106e8292a7f
Breimerct98 <breimer.correa@zabud.com.co>
2024-03-24 12:54:32 -0500

    Agregadas interfaces TaskDto y UserDto

---
+++ src/dto/task.dto.ts
@@ -0,0 +1,7 @@
+interface TaskDto {
+    id?: string;
+    userId: string;
+    title: string;
+    description: string;
+    status: string;
+}
---
+++ src/dto/user.dto.ts
@@ -0,0 +1,8 @@
+interface UserDto {
+    id?: string;
+    name: string;
+    lastname: string;
+    fullname: string;
+    email: string;
+    password: string;
+}
