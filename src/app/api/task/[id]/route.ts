af5de4f1c1a11834aca585d2e7dfec51875f94a2
Breimerct98 <breimer.correa@zabud.com.co>
2024-03-24 16:58:22 -0500

    Agregadas funciones POST, DELETE y PATCH a la ruta de la tarea con ID. Agregada función GET a la ruta de la tarea.

---
+++ src/app/api/task/[id]/route.ts
@@ -0,0 +1,5 @@
+export async function POST(request: Request, response: { params: { id: string } }) {}
+
+export async function DELETE(request: Request, response: { params: { id: string } }) {}
+
+export async function PATCH(request: Request, response: { params: { id: string } }) {}
