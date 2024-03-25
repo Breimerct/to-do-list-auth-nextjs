10272bedc10be0e665830bad8aad5e2d6ba474ba
Breimerct98 <breimer.correa@zabud.com.co>
2024-03-25 16:27:32 -0500

    Agrega rutas protegidas y lista de tareas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import TaskList from "@/components/task-list";
+
 export default async function Home() {
-    return <main className="min-h-screen min-w-max"></main>;
+    return (
+        <main className="min-h-screen min-w-max">
+            <TaskList />
+        </main>
+    );
 }
