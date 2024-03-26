3975cdfd9a24424ae0ec95a74d5ef39d2cb33cad
Breimerct98 <breimer.correa@zabud.com.co>
2024-03-26 03:25:12 -0500

    Agregado enlace a la página principal en el encabezado

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
 import UserLogged from "./user-logged";
 
 const Header = () => {
     return (
         <header className="w-full p-4 flex justify-between items-center shadow-md mb-4">
-            <h1 className="text-2xl font-bold">To Do List</h1>
+            <Link href="/">
+                <h1 className="text-2xl font-bold">To Do List</h1>
+            </Link>
 
             <UserLogged />
         </header>
