9f497b68c48dc0f429459bf7a97804af58711384
Breimerct98 <breimer.correa@zabud.com.co>
2024-03-26 12:27:36 -0500

    Actualizar estilos de sombra en las páginas de inicio de sesión y registro

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "@/components/login-form";
 
 const Login = () => {
     return (
-        <div className="p-4 shadow-md w-full max-w-[30rem]">
+        <div className="p-4 shadow-2xl w-full max-w-[30rem]">
             <h2 className="text-center font-bold text-3xl mb-4">Inicio de sesión</h2>
             <LoginForm />
         </div>
--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -2,7 +2,7 @@ import RegisterForm from "@/components/register-form";
 
 const Register = () => {
     return (
-        <div className="p-4 shadow-md w-full max-w-[30rem]">
+        <div className="p-4 shadow-xl w-full max-w-[30rem]">
             <h2 className="text-center font-bold text-3xl mb-4">Registro</h2>
             <RegisterForm />
         </div>
